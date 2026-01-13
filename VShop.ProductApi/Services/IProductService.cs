0d9a56730df3dd14426cf2e90087960de0889ea1
Ricardo <ricardo.formagio@amlabs.com.br>
2026-01-13 15:38:52 -0300

    Add ProductService and IProductService for product management

---
+++ VShop.ProductApi/Services/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using VShop.ProductApi.DTOs;
+
+namespace VShop.ProductApi.Services;
+
+public interface IProductService
+{
+    Task<IEnumerable<ProductDTO>> GetAllProducts();
+    Task<ProductDTO> GetProductById(int id);
+    Task AddProduct(ProductDTO productDto);
+    Task UpdateProduct(ProductDTO productDto);
+    Task RemoveProduct(int id);
+}
