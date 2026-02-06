b01ac67f97d0f43e335da0161d4d3ea53784bd78
Ricardo <ricardo.formagio@amlabs.com.br>
2026-02-06 17:46:28 -0300

    Add OpenID Connect authentication to Web project, configure IdentityServer integration, and update project dependencies

--- VShop.Web/Program.cs
+++ VShop.Web/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication;
 using VShop.Web.Services;
 using VShop.Web.Services.Contracts;
 
@@ -11,6 +12,34 @@ builder.Services.AddHttpClient("ProductApi", c =>
 });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = "Cookies";
+        options.DefaultChallengeScheme = "oidc";
+    })
+    .AddCookie("Cookies", c =>
+    {
+        c.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+    })
+    .AddOpenIdConnect("oidc", options =>
+        {
+            options.Authority = builder.Configuration["ServiceUri:IdentityServer"];
+            options.GetClaimsFromUserInfoEndpoint = true;
+
+            options.ClientId = "vshop";
+            options.ClientSecret = builder.Configuration["Client:Secret"];
+            options.ResponseType = "code";
+
+            options.ClaimActions.MapJsonKey("role", "role", "role");
+            options.ClaimActions.MapJsonKey("sub", "sub", "sub");
+
+            options.TokenValidationParameters.NameClaimType = "name";
+            options.TokenValidationParameters.RoleClaimType = "role";
+
+            options.Scope.Add("vshop");
+            options.SaveTokens = true;
+        }
+    );
 
 var app = builder.Build();
 
@@ -24,7 +53,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
