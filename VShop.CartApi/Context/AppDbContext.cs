6f03377b49b09ac78f41999cb7bf8fffede56982
Ricardo <ricardo.formagio@amlabs.com.br>
2026-02-09 16:12:34 -0300

    Add Cart API models and database context for product and cart management

---
+++ VShop.CartApi/Context/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VShop.CartApi.Models;
+
+namespace VShop.CartApi.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    
+    public DbSet<Product>? Products { get; set; }
+    public DbSet<CartItem>? CartItems { get; set; }
+    public DbSet<CartHeader>? CartHeaders { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+        //Products
+        mb.Entity<Product>().HasKey(c => c.Id);
+        mb.Entity<Product>().Property(c => c.Id).ValueGeneratedNever();
+        mb.Entity<Product>().Property(p => p.Name).HasMaxLength(100).IsRequired();
+        mb.Entity<Product>().Property(p => p.Description).HasMaxLength(255).IsRequired();
+        mb.Entity<Product>().Property(p => p.ImageURL).HasMaxLength(255).IsRequired();
+        mb.Entity<Product>().Property(p => p.Price).HasPrecision(12, 2).IsRequired();
+        
+        //CartHeader
+        mb.Entity<CartHeader>().Property(c => c.UserId).HasMaxLength(255).IsRequired();
+        mb.Entity<CartHeader>().Property(c => c.CouponCode).HasMaxLength(100);
+    }
+}
